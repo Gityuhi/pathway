6a63944d4c4d69b49488af4c54d8de1908a9537c
Gityuhi <yuhiprivate@gmail.com>
2026-06-07 16:17:38 +0900

    feat: server起動時にpgxpoolでpostgres接続を追加

--- backend/go.mod
+++ backend/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/sosodev/duration v1.4.0 // indirect
 	github.com/urfave/cli/v3 v3.8.0 // indirect
 	golang.org/x/mod v0.33.0 // indirect
--- backend/server.go
+++ backend/server.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -13,12 +14,31 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	dbSource := os.Getenv("DB_SOURCE")
+	if dbSource == "" {
+		log.Fatal("DB_SOURCE is not set")
+	}
+	config, err := pgxpool.ParseConfig(dbSource)
+	if err != nil {
+		log.Fatal("cannot parse db source:", err)
+	}
+	config.MaxConns = 10
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	log.Println("Connected to db")
+	defer pool.Close()
+
 	port := os.Getenv("API_PORT")
 	if port == "" {
 		port = defaultPort
